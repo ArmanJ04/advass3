9664c4b36208ce4faeff2269f775815e8feeddeb
ArmanJ04 <dzhansatov00@gmail.com>
2024-01-30 16:19:56 +0600

    Initial commit

--- go.mod
+++ go.mod
@@ -3,7 +3,24 @@ module advanced3
 go 1.21
 
 require (
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gorilla/mux v1.8.1
+	gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.6
+)
+
+require (
+	github.com/didip/tollbooth v4.0.2+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,51 +1,70 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
 	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"html/template"
-	"log"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type Product struct {
-	ID    int
+	gorm.Model
 	Name  string
 	Price float64
 }
 
-var db *sql.DB
+var db *gorm.DB
+var log *logrus.Logger
 
 func main() {
+	initLogger()
 	initDB()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleIndex)
+	lim := tollbooth.NewLimiter(1, &limiter.ExpirableOptions{DefaultExpirationTTL: 5 * time.Second})
+	r.Handle("/", tollbooth.LimitHandler(lim, http.HandlerFunc(handleIndex)))
+	r.Handle("/register", tollbooth.LimitHandler(lim, http.HandlerFunc(handleRegister))).Methods("POST")
+	r.Handle("/update", tollbooth.LimitHandler(lim, http.HandlerFunc(handleUpdate))).Methods("POST")
+	r.Handle("/delete", tollbooth.LimitHandler(lim, http.HandlerFunc(handleDelete))).Methods("POST")
+
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func initLogger() {
+	log = logrus.New()
+	log.SetFormatter(&logrus.JSONFormatter{})
+	file, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(file)
+	} else {
+		log.Error("Failed to open log file:", err)
+	}
+	log.SetOutput(io.MultiWriter(os.Stdout, file))
+}
+
 func initDB() {
 	var err error
-	db, err = sql.Open("postgres", "user=postgres password=jansatov04 dbname=postgres sslmode=disable host=localhost port=3000")
+	dsn := "user=postgres password=jansatov04 dbname=postgres sslmode=disable host=localhost port=3000"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error initializing database:", err)
 	}
-
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS products (
-			id SERIAL PRIMARY KEY,
-			name VARCHAR(255),
-			price DECIMAL(10, 2)
-		)
-	`)
-	if err != nil {
-		log.Fatal(err)
-	}
+	db.AutoMigrate(&Product{})
 }
+
 func handleIndex(w http.ResponseWriter, r *http.Request) {
+	var products []Product
+
 	filter := r.URL.Query().Get("filter")
 	sort := r.URL.Query().Get("sort")
 	pageStr := r.URL.Query().Get("page")
@@ -53,49 +72,166 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 	if err != nil || page < 1 {
 		page = 1
 	}
-	query := "SELECT * FROM products"
+
+	query := db
 	if filter != "" {
-		query += fmt.Sprintf(" WHERE name LIKE '%%%s%%'", filter)
+		query = query.Where("name LIKE ?", "%"+filter+"%")
 	}
-	if sort != "" {
-		query += fmt.Sprintf(" ORDER BY %s", sort)
+
+	switch {
+	case strings.HasPrefix(sort, "name"):
+		query = query.Order("name " + strings.TrimPrefix(sort, "name_"))
+	case strings.HasPrefix(sort, "price"):
+		query = query.Order("price " + strings.TrimPrefix(sort, "price_"))
 	}
+
 	limit := 10
 	offset := (page - 1) * limit
-	query += fmt.Sprintf(" LIMIT %d OFFSET %d", limit, offset)
-	rows, err := db.Query(query)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	var products []Product
-	for rows.Next() {
-		var product Product
-		err := rows.Scan(&product.ID, &product.Name, &product.Price)
-		if err != nil {
-			log.Fatal(err)
-		}
-		products = append(products, product)
-	}
-	renderTemplate(w, products, filter, sort, page)
+	query = query.Limit(limit).Offset(offset)
+
+	query.Find(&products)
+
+	renderTemplate(w, products, filter, sort, page, "", "")
 }
 
-func renderTemplate(w http.ResponseWriter, products []Product, filter, sort string, page int) {
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	priceStr := r.FormValue("price")
+
+	price, err := strconv.ParseFloat(priceStr, 64)
+	if err != nil {
+		renderTemplate(w, nil, "", "", 1, "", "Invalid price")
+		log.WithFields(logrus.Fields{
+			"action": "register",
+			"status": "failure",
+			"error":  "Invalid price",
+		}).Error("Failed to register product")
+		return
+	}
+
+	newProduct := Product{Name: name, Price: price}
+	db.Create(&newProduct)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	log.WithFields(logrus.Fields{
+		"action":  "register",
+		"status":  "success",
+		"product": newProduct,
+	}).Info("Product registered successfully")
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+	productIDStr := r.FormValue("productIdUpdate")
+	newName := r.FormValue("newName")
+	newPriceStr := r.FormValue("newPrice")
+
+	productID, err := strconv.ParseUint(productIDStr, 10, 64)
+	if err != nil {
+		renderTemplate(w, nil, "", "", 1, "", "Invalid Product ID")
+		log.WithFields(logrus.Fields{
+			"action": "update",
+			"status": "failure",
+			"error":  "Invalid Product ID",
+		}).Error("Failed to update product")
+		return
+	}
+
+	var updateProduct Product
+	result := db.First(&updateProduct, productID)
+	if result.Error != nil {
+		renderTemplate(w, nil, "", "", 1, "", "Product not found")
+		log.WithFields(logrus.Fields{
+			"action": "update",
+			"status": "failure",
+			"error":  "Product not found",
+		}).Error("Failed to update product")
+		return
+	}
+
+	if newName != "" {
+		updateProduct.Name = newName
+	}
+	if newPriceStr != "" {
+		newPrice, err := strconv.ParseFloat(newPriceStr, 64)
+		if err != nil {
+			renderTemplate(w, nil, "", "", 1, "", "Invalid price")
+			log.WithFields(logrus.Fields{
+				"action": "update",
+				"status": "failure",
+				"error":  "Invalid price",
+			}).Error("Failed to update product")
+			return
+		}
+		updateProduct.Price = newPrice
+	}
+	db.Save(&updateProduct)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	log.WithFields(logrus.Fields{
+		"action":  "update",
+		"status":  "success",
+		"product": updateProduct,
+	}).Info("Product updated successfully")
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	productIDStr := r.FormValue("productIdDelete")
+
+	productID, err := strconv.ParseUint(productIDStr, 10, 64)
+	if err != nil {
+		renderTemplate(w, nil, "", "", 1, "", "Invalid Product ID")
+		log.WithFields(logrus.Fields{
+			"action": "delete",
+			"status": "failure",
+			"error":  "Invalid Product ID",
+		}).Error("Failed to delete product")
+		return
+	}
+
+	var deleteProduct Product
+	result := db.First(&deleteProduct, productID)
+	if result.Error != nil {
+		renderTemplate(w, nil, "", "", 1, "", "Product not found")
+		log.WithFields(logrus.Fields{
+			"action": "delete",
+			"status": "failure",
+			"error":  "Product not found",
+		}).Error("Failed to delete product")
+		return
+	}
+
+	db.Delete(&deleteProduct, productID)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	log.WithFields(logrus.Fields{
+		"action":  "delete",
+		"status":  "success",
+		"product": deleteProduct,
+	}).Info("Product deleted successfully")
+}
+
+func renderTemplate(w http.ResponseWriter, products []Product, filter, sort string, page int, successMsg, errorMsg string) {
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	data := struct {
-		Products []Product
-		Filter   string
-		Sort     string
-		Page     int
+		Products   []Product
+		Filter     string
+		Sort       string
+		Page       int
+		SuccessMsg string
+		ErrorMsg   string
 	}{
-		Products: products,
-		Filter:   filter,
-		Sort:     sort,
-		Page:     page,
+		Products:   products,
+		Filter:     filter,
+		Sort:       sort,
+		Page:       page,
+		SuccessMsg: successMsg,
+		ErrorMsg:   errorMsg,
 	}
+
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
